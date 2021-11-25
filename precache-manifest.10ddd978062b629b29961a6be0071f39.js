f7239e1654cfaaed4f06587c8dabdfe774b33f7d
Ami Hazbany <ami@orbs.com>
2021-11-25 22:08:54 +0200

    Updates

---
+++ precache-manifest.10ddd978062b629b29961a6be0071f39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e2b0ea7c238b77f362c50baa0abbae",
+    "url": "https://orbs-network.github.io/v2-matic-event-explorer/index.html"
+  },
+  {
+    "revision": "7439f802a2a70af54ed8",
+    "url": "https://orbs-network.github.io/v2-matic-event-explorer/static/css/main.d4b27514.chunk.css"
+  },
+  {
+    "revision": "6d4d8b20232513825722",
+    "url": "https://orbs-network.github.io/v2-matic-event-explorer/static/js/2.3627cd71.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "https://orbs-network.github.io/v2-matic-event-explorer/static/js/2.3627cd71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7439f802a2a70af54ed8",
+    "url": "https://orbs-network.github.io/v2-matic-event-explorer/static/js/main.8bd836b5.chunk.js"
+  },
+  {
+    "revision": "97c68de9b3e28af1c69d",
+    "url": "https://orbs-network.github.io/v2-matic-event-explorer/static/js/runtime-main.e0f2b145.js"
+  }
+]);
